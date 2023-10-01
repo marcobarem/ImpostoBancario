ef9705db8f7e5de8a69418e579c1f5bdd6030d08
Barem <marcobarem@gmail.com>
2023-09-30 23:20:07 -0300

    Ex POO

---
+++ src/ContaCorrente.java
@@ -0,0 +1,10 @@
+public class ContaCorrente extends Conta implements Tributavel{
+
+    @Override
+    public double calculaTributos() {
+        double tributo = saldo * 0.01;
+        return tributo;
+
+
+    }
+}
